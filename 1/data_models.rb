1e195ae8cfe6521730c106307504c29b81dcd9d4
Jeremy <searls@me.com>
2015-12-01 13:31:00 -0500

    Loops over CSV files to create county std data
    example for irb:
    
    require_relative 'data_models'; CreatesCountyStdModels.new("csv/county_data").create

--- 1/data_models.rb
+++ 1/data_models.rb
@@ -1,9 +1,42 @@
-class CountySTD
-  def initialize() # <-- field1,field2,field3,etc... as arguments
-    # intialize class variables
+require 'csv'
+class CreatesModelsFromCsv
+  def initialize(csv_path, model_class)
+    @csv_path = csv_path
+    @model_class = model_class
   end
 
-  # Add any additional class/instance methods here
+  def create
+    CSV.foreach(@csv_path,
+      headers: true,
+      header_converters: ->(header) { header.downcase.gsub(/[\W]+/, '_') }
+    ).map do |row|
+      @model_class.new.tap do |model|
+        puts row.headers
+        row.headers.each do |attr|
+          model.send("#{attr}=", row[attr])
+        end
+      end
+    end
+  end
+end
+
+class CreatesCountyStdModels
+  def initialize(directory)
+    @directory = directory
+  end
+
+  def create
+    Dir["#{@directory}/*.csv"].map do |path|
+      CreatesModelsFromCsv.new(path, CountySTD).create
+    end
+  end
+end
+
+class CountySTD
+  attr_accessor :year, :geography,
+                :chlamydia_cases, :chlamydia_rate,
+                :gonorrhea_cases, :gonorrhea_rate,
+                :primary_and_secondary_syphilis_cases, :primary_and_secondary_syphilis_rate
 end
 
 class CountyFIPS
