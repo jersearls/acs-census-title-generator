0e6d37263dd93c446070c088177ba5c4229790e8
Jeremy <searls@me.com>
2015-12-01 12:13:25 -0500

    Initial commit
    * Initially received files
    * Added fips code data
    * Added county data

---
+++ 1/data_models.rb
@@ -0,0 +1,23 @@
+class CountySTD
+  def initialize() # <-- field1,field2,field3,etc... as arguments
+    # intialize class variables
+  end
+
+  # Add any additional class/instance methods here
+end
+
+class CountyFIPS
+  def initialize(state_abrev,state_code,county_code,county_name,county_class)
+    # intialize class variables
+  end
+
+  # Add any additional class/instance methods here
+end
+
+class CountyJoined
+  def initialize()
+    # intialize class variables
+  end
+
+  # Add any additional class/instance methods here
+end
---
+++ 1/script.rb
@@ -0,0 +1,20 @@
+require './Challenge_1_Data_Classes'
+require 'csv'
+
+class CountyData
+  def initialize()
+    # intialize class variables
+  end
+
+  def process()
+    # import and join data
+    # write data to one .csv
+    # this method may call other helper methods
+  end
+
+  # Add any additional class/instance methods here
+end
+
+#
+# cd = CountyData.new()
+# cd.process
