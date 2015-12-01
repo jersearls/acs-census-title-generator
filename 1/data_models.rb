f0a2f35f2333f5c313672e94e28520a32d9e3fe7
Jeremy <searls@me.com>
2015-12-01 13:47:51 -0500

    Store the state with each CountySTD & flatten it

--- 1/data_models.rb
+++ 1/data_models.rb
@@ -10,11 +10,16 @@ class CreatesModelsFromCsv
       headers: true,
       header_converters: ->(header) { header.downcase.gsub(/[\W]+/, '_') }
     ).map do |row|
-      @model_class.new.tap do |model|
-        puts row.headers
-        row.headers.each do |attr|
-          model.send("#{attr}=", row[attr])
-        end
+      model_from_row(row)
+    end
+  end
+
+private
+
+  def model_from_row(row)
+    @model_class.new.tap do |model|
+      row.headers.each do |attr|
+        model.send("#{attr}=", row[attr])
       end
     end
   end
@@ -27,13 +32,16 @@ class CreatesCountyStdModels
 
   def create
     Dir["#{@directory}/*.csv"].map do |path|
-      CreatesModelsFromCsv.new(path, CountySTD).create
-    end
+      state = path.match(/#{@directory}\/(.*)\.csv/)[1].upcase
+      CreatesModelsFromCsv.new(path, CountySTD).create.tap do |models|
+        models.each { |m| m.state = state }
+      end
+    end.flatten
   end
 end
 
 class CountySTD
-  attr_accessor :year, :geography,
+  attr_accessor :state, :year, :geography,
                 :chlamydia_cases, :chlamydia_rate,
                 :gonorrhea_cases, :gonorrhea_rate,
                 :primary_and_secondary_syphilis_cases, :primary_and_secondary_syphilis_rate
