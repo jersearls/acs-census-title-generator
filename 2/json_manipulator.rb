8ccf487ac892306ef66df658dfaa2ea2331f7898
Jeremy <searls@me.com>
2015-12-03 16:56:19 -0500

    Creates new Key:value w/new value names

--- 2/json_manipulator.rb
+++ 2/json_manipulator.rb
@@ -1,8 +1,14 @@
 require 'json'
 
-my_file = File.read('county.json')
+county_string = File.read('county.json')
 
-my_hash = begin
-  JSON.parse(my_file)
-rescue JSON::ParserError
+county_hash =  JSON.parse(county_string)
+
+def update_statefp(state_id)
+    if state_id == "01"
+      state_id =  "Alaska"
+  else state_id = state_id
+  end
 end
+
+county_hash.map { |o| o["properties"]["state_and_county"] = update_statefp(o["properties"]["STATEFP"]) + "-" + o["properties"]["NAME"]}
