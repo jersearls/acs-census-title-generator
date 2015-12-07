bcc2a29f927ffa05d19d93705c0cfa6648e46e62
Jeremy <searls@me.com>
2015-12-07 14:21:00 -0500

    Changed method to hash for state abbreviation

--- 2/json_manipulator.rb
+++ 2/json_manipulator.rb
@@ -4,168 +4,22 @@ county_string = File.read('county.json')
 
 county_hash =  JSON.parse(county_string)
 
-def update_statefp(state_id)
-    if state_id == "01"
-      state_id = "AL"
-  elsif
-  state_id == "02"
-    state_id = "AK"
-  elsif
-  state_id == "02"
-    state_id = "AK"
-  elsif
-  state_id == "04"
-    state_id = "AZ"
-  elsif
-  state_id == "05"
-    state_id = "AR"
-  elsif
-  state_id == "06"
-    state_id = "CA"
-  elsif
-  state_id == "08"
-    state_id = "CO"
-  elsif
-  state_id == "09"
-    state_id = "CT"
-  elsif
-  state_id == "10"
-    state_id = "DE"
-  elsif
-  state_id == "11"
-    state_id = "DC"
-  elsif
-  state_id == "12"
-    state_id = "FL"
-  elsif
-  state_id == "13"
-    state_id = "GA"
-  elsif
-  state_id == "15"
-    state_id = "HI"
-  elsif
-  state_id == "16"
-    state_id = "ID"
-  elsif
-  state_id == "17"
-    state_id = "IL"
-  elsif
-  state_id == "18"
-    state_id = "IN"
-  elsif
-  state_id == "19"
-    state_id = "IA"
-  elsif
-  state_id == "20"
-    state_id = "KS"
-  elsif
-  state_id == "21"
-    state_id = "KY"
-  elsif
-  state_id == "22"
-    state_id = "LA"
-  elsif
-  state_id == "23"
-    state_id = "ME"
-  elsif
-  state_id == "24"
-    state_id = "MD"
-  elsif
-  state_id == "25"
-    state_id = "MA"
-  elsif
-  state_id == "MI"
-    state_id = "26"
-  elsif
-  state_id == "27"
-    state_id = "MN"
-  elsif
-  state_id == "28"
-    state_id = "MS"
-  elsif
-  state_id == "29"
-    state_id = "MO"
-  elsif
-  state_id == "30"
-    state_id = "MT"
-  elsif
-  state_id == "31"
-    state_id = "NE"
-  elsif
-  state_id == "32"
-    state_id = "NV"
-  elsif
-  state_id == "33"
-    state_id = "NH"
-  elsif
-  state_id == "34"
-    state_id = "NJ"
-  elsif
-  state_id == "35"
-    state_id = "NM"
-  elsif
-  state_id == "36"
-    state_id = "NY"
-  elsif
-  state_id == "37"
-    state_id = "NC"
-  elsif
-  state_id == "38"
-    state_id = "ND"
-  elsif
-  state_id == "39"
-    state_id = "OH"
-  elsif
-  state_id == "40"
-    state_id = "OK"
-  elsif
-  state_id == "41"
-    state_id = "OR"
-  elsif
-  state_id == "42"
-    state_id = "PA"
-  elsif
-  state_id == "44"
-    state_id = "RI"
-  elsif
-  state_id == "45"
-    state_id = "SC"
-  elsif
-  state_id == "46"
-    state_id = "SD"
-  elsif
-  state_id == "47"
-    state_id = "TN"
-  elsif
-  state_id == "48"
-    state_id = "TX"
-  elsif
-  state_id == "49"
-    state_id = "UT"
-  elsif
-  state_id == "50"
-    state_id = "VT"
-  elsif
-  state_id == "51"
-    state_id = "VA"
-  elsif
-  state_id == "53"
-    state_id = "WA"
-  elsif
-  state_id == "54"
-    state_id = "WV"
-  elsif
-  state_id == "55"
-    state_id = "WI"
-  elsif
-  state_id == "56"
-    state_id = "WY"
-  else state_id = state_id
-  end
-end
+state_hash = { "01" => "AL", "02" => "AK", "04" => "AZ", "05" => "AR",
+  "06" => "CA", "08" => "CO", "09" => "CT", "10" => "DE", "11" => "DC",
+  "12" => "FL", "13" => "GA", "15" => "HI", "16" => "ID", "17" => "IL",
+  "18" => "IN", "19" => "IA", "20" => "KS", "21" => "KY", "22" => "LA",
+  "23" => "ME", "24" => "MD", "25" => "MA", "26" => "MI", "27" => "MN",
+  "28" => "MS", "29" => "MO", "30" => "MT", "31" => "NE", "32" => "NV",
+  "33" => "NH", "34" => "NJ", "35" => "NM", "36" => "NY", "37" => "NC",
+  "38" => "ND", "39" => "OH", "40" => "OK", "41" => "OR", "42" => "PA",
+  "44" => "RI", "45" => "SC", "46" => "SD", "47" => "TN", "48" => "TX",
+  "49" => "UT", "50" => "VT", "51" => "VA", "53" => "WA", "54" => "WV",
+  "55" => "WI", "56" => "WY", "72" => "PR"}
 
-county_hash.map { |o| o["properties"]["state_and_county"] = update_statefp(o["properties"]["STATEFP"]) + "-" + o["properties"]["NAME"]}
 
-File.open("test.json","w") do |f|
+county_hash.map { |o| o["properties"]["state_and_county"] =
+  state_hash[o["properties"]["STATEFP"]] + "-" + o["properties"]["NAME"]}
+
+File.open("test1.json","w") do |f|
   f.write(JSON.pretty_generate(county_hash))
 end
