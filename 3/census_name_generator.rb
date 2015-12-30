fb5eedbe87676a4d35214156fd17223555054e59
Jeremy <searls@me.com>
2015-12-30 12:10:38 -0500

    make method to load csv

--- 3/census_name_generator.rb
+++ 3/census_name_generator.rb
@@ -1,1 +1,19 @@
 require 'csv'
+require 'pry'
+
+def load_csv(filename)
+  CSV::Converters[:blank_to_nil] = lambda do |field|
+    field && field.empty? ? nil : field
+  end
+  CSV.new(File.read(filename), 
+    :headers => true, 
+    :header_converters => :symbol, 
+    :converters => [:all, :blank_to_nil]
+    )
+end
+
+testcsv = load_csv('columndatatest.csv')
+
+
+
+binding.pry
