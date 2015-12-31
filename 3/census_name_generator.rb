a78ff88a7b9c49e2874bc33ee3fef0a5dd08d07f
Jeremy <searls@me.com>
2015-12-31 12:15:01 -0500

    added column_id generator method

--- 3/census_name_generator.rb
+++ 3/census_name_generator.rb
@@ -15,33 +15,42 @@ def load_csv(filename)
 
 #creates a new hash with the column id as the key and an array containing column title and a 
 #nested array of the parent hash
-def create_column_types(filename)
-column_types = {}
+def create_column_titles(filename)
+column_titles = {}
 	
 	load_csv(filename).map do |row|
-	column_types[row[:column_id]] =
+	column_titles[row[:column_id]] =
 	row[:column_title],
-	column_types[row[:parent_column_id]]
+	column_titles[row[:parent_column_id]]
+	end
+
+end
+
+def create_column_id(filename)
+	
+	load_csv(filename).map do |row|
+	row[:column_id]
 	end
 
 end
 
 #calls method and saves output of .map
 
-column_types = create_column_types('columndatatest.csv')
+column_titles = create_column_titles('columndatatest.csv')
+
+column_id = create_column_id('columndatatest.csv')
 
 #displays an array of only the titles in the correct logical order	
-processed_titles = column_types.map { |o|
+processed_titles = column_titles.map { |o|
 		o.flatten.compact.reverse.join
 	}
 
-puts processed_titles
+
 
 binding.pry
 
 #########################
-# Questions:
-# How can I save the values that are created as the csv is iterated over? CLass?
-# Then call those values to simply concatenate into logical string
+# To do
+# Output to a csv with column id | full name
 
 
