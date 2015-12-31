e785aee3659b2e2bb91bb0712ac2d722223d7d36
Jeremy <searls@me.com>
2015-12-31 10:46:21 -0500

    used .map instead of .each to save output of iterating through csv hash outside the method

--- 3/census_name_generator.rb
+++ 3/census_name_generator.rb
@@ -1,7 +1,6 @@
 require 'csv'
 
 #loads csv file and creates a hash from each row
-
 def load_csv(filename)
   CSV::Converters[:blank_to_nil] = lambda do |field|
     field && field.empty? ? nil : field
@@ -11,35 +10,32 @@ def load_csv(filename)
     :header_converters => :symbol, 
     :converters => [:all, :blank_to_nil]
   ).entries.map { |row| row.to_hash }
-end
+	end
 
 #creates a new hash with the column id as the key and an array containing column title and a 
 #nested array of the parent hash
 def create_column_types(filename)
 column_types = {}
 	
-	load_csv(filename).each do |row|
-	column_types[row[:column_id]] = 
+	load_csv(filename).map do |row|
+	column_types[row[:column_id]] =
 	row[:column_title],
 	column_types[row[:parent_column_id]]
-	
-
 	end
-
-#displays an array of only the titles in the correct logical order
-	puts column_types.values.flatten.reverse
-
-
 
 end
 
-#calls method
+#calls method and saves output of .map
 
-create_column_types('columndatatest.csv')
+column_types = create_column_types('columndatatest.csv')
+
+#displays an array of only the titles in the correct logical order	
+	puts column_types.flatten.reverse
+
 
 #########################
 # Questions:
-# How can I save the values that are created as the csv is iterated over?
+# How can I save the values that are created as the csv is iterated over? CLass?
 # Then call those values to simply concatenate into logical string
 
 
