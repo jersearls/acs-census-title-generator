cb107fa9cd758a8bb965b88b63c0ea11f3daec53
Jeremy <searls@me.com>
2016-01-04 16:25:14 -0500

    created array of table names with table_id

--- 3/lib/table_data.rb
+++ 3/lib/table_data.rb
@@ -4,17 +4,25 @@ require 'pry'
 
 
 #creates an array of full table titles
-def create_table_name(filename)
+def create_table_info(filename)
+	table = []
 	load_csv(filename).map do |row|
-	row[:table_title]
+	table = [row[:table_title], row[:table_id]]
 	end
-
 end
 
 #calls and assigns table_name array
-def get_table_name
+def get_table_info
 	table_name = 
 	create_table_name('../data/census_table_metadata.csv')
 end
+
+column_array = create_column_data_array
+
+column_array.map{ |columns|
+	if o[1] == 
+}
+
+
 
 binding.pry
