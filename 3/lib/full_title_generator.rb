a01696300b2e21bbb138c7e1eb4a1c36893bb186
Jeremy <searls@me.com>
2016-01-05 14:54:42 -0500

    reformatted titles to snake_case

--- 3/lib/full_title_generator.rb
+++ 3/lib/full_title_generator.rb
@@ -14,13 +14,27 @@ def get_table_info
 	create_table_info('../data/census_table_metadata.csv')
 end
 
-def create_columns_with_table_names
+
+def combine_columns_and_table_names
 	table_names = get_table_info
 	columns = create_column_data
 	columns.each do |column|
-		column[:full_title] = table_names[column[:table_id]] + column[:title]
+		column[:full_title] = "#{table_names[column[:table_id]]}_#{column[:title]}"
 	end
 	return columns
 end
+
+def reformat_titles
+	title_info = combine_columns_and_table_names
+	column_hash = {}
+	title_info.map do |info| 
+		column_hash = {title: info[:full_title].downcase.gsub(/[^A-Za-z0-9,:,.]+/,'_').
+		gsub(/^_/, '').gsub(/_$/, ''), table_id: info[:table_id], col_id: info[:col_id]}
+	end
+	
+end
+
+
+
 
 binding.pry
