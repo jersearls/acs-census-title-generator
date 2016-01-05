d62d0a8768f778821baa223e2c54f08f47056c2b
Jeremy <searls@me.com>
2016-01-05 14:26:08 -0500

    creates concatenated title

--- 3/lib/full_title_generator.rb
+++ 3/lib/full_title_generator.rb
@@ -2,26 +2,25 @@ require_relative '../lib/column_name_generator.rb'
 require 'csv'
 require 'pry'
 
-
 #creates a hash with table id as the key and title as the value
 def create_table_info(filename)
-	table_hash = {}
-	load_csv(filename).map do |row|
-	table_hash = {row[:table_id] => row[:table_title]}
-	end
+	Hash[load_csv(filename).map do |row|
+		[row[:table_id], row[:table_title]]
+	end]
 end
 
 #calls and assigns table_name array
 def get_table_info
-	table_name = 
 	create_table_info('../data/census_table_metadata.csv')
 end
 
-def add_table_title_to_col_array
-	create_column_data_hash.map do |hash| hash[0][:table_id] = 
-		get_table_info
+def create_columns_with_table_names
+	table_names = get_table_info
+	columns = create_column_data
+	columns.each do |column|
+		column[:full_title] = table_names[column[:table_id]] + column[:title]
 	end
-	
+	return columns
 end
 
 binding.pry
