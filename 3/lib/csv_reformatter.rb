6e1f0311d9197d3803bf1bcf2c737f84c35a9031
Jeremy <searls@me.com>
2016-01-06 11:00:39 -0500

    added new hash of entries for margin of error columns

--- 3/lib/csv_reformatter.rb
+++ 3/lib/csv_reformatter.rb
@@ -12,9 +12,7 @@ def create_estimate_hash
 	  
 		titles[:col_id].slice!(-3) if titles[:col_id][-3] == "0"
 
-		titles[:col_id].slice!(-2) if titles[:col_id][-2] == "0"
-		
-		
+		titles[:col_id].slice!(-2) if titles[:col_id][-2] == "0"	
 	end
 
 	return titles
@@ -24,6 +22,18 @@ end
 
 def create_margin_of_error_hash
 	titles = reformat_titles
+
+	titles.map do |titles| 
+		titles[:col_id].insert(-4, 'm')
+	  
+		titles[:col_id].slice!(-3) if titles[:col_id][-3] == "0"
+
+		titles[:col_id].slice!(-2) if titles[:col_id][-2] == "0"
+
+		titles[:title].insert(0, 'margin_of_error:')
+	end
+
+	return titles
 end
 
 
