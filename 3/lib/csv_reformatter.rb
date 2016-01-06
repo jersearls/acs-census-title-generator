873270c7bf0c451c322173740dbd388e12370e96
Jeremy <searls@me.com>
2016-01-05 22:53:41 -0500

    formats column id to match census tables

---
+++ 3/lib/csv_reformatter.rb
@@ -0,0 +1,30 @@
+
+require_relative '../lib/column_name_generator.rb'
+require_relative '../lib/full_title_generator.rb'
+require 'csv'
+require 'pry'
+
+def create_estimate_hash
+	titles = reformat_titles
+
+	titles.map do |titles| 
+		titles[:col_id].insert(-4, 'e')
+	  
+		titles[:col_id].slice!(-3) if titles[:col_id][-3] == "0"
+
+		titles[:col_id].slice!(-2) if titles[:col_id][-2] == "0"
+		
+		
+	end
+
+	return titles
+			
+
+end
+
+def create_margin_of_error_hash
+	titles = reformat_titles
+end
+
+
+binding.pry
