50558f539d9ca93bbdbfa9b0cebc6345f7656793
Jeremy <searls@me.com>
2016-01-07 10:04:04 -0500

    added margin of error to csv, removed unused method

--- 3/script/driver.rb
+++ 3/script/driver.rb
@@ -9,6 +9,7 @@ def csv_header_alteration
 	input = File.open('../data/sf0039wv.csv', 'r')
 	output = File.open('../data/TESTcsv.csv', 'w')
 	estimate = create_hash(format_estimate_array)
+	margin = create_hash(format_margin_of_error_array)
 
 	CSV.filter(input, output, :headers => true, 
 		:write_headers => true, :return_headers => true) do |row|
@@ -16,22 +17,16 @@ def csv_header_alteration
 			row.headers.each_with_index do |column_id, i|
 				if estimate.has_key?(column_id)
 					row[i] = estimate[column_id]
+				elsif margin.has_key?(column_id)
+					row[i] = margin[column_id]
 				end
 			end
 		end
 	end
-
 	input.close
 	output.close
-	
-
 end
 
-
-def change_headers(row)
-	estimate = create_hash(format_estimate_array)
-
-end
 
 csv_header_alteration
 
