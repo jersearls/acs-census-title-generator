0509e80829ec98f60068c72fda922bdc0e22cf4e
Jeremy <searls@me.com>
2016-01-07 11:27:57 -0500

    accepts folder of csvs for conversion and marks their progree

--- 3/script/driver.rb
+++ 3/script/driver.rb
@@ -5,7 +5,18 @@ require_relative '../lib/csv_generator.rb'
 require 'csv'
 require 'pry'
 
-csv_header_alteration('../data/sf0035ia.csv')
+files = Dir.entries('../data/acs')
+files = files[2..-1]
+p files
+puts files.length
+
+files.each do |file|
+  csv_header_alteration('../data/acs/'+file)
+  puts "One more done!"
+  p file
+end
+
+
 
 
 
