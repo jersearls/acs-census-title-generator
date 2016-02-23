1784995df7ff65fa4231b6500e028252a76df8a9
Jeremy <searls@me.com>
2016-02-23 14:47:17 -0500

    created driver class

--- script/driver.rb
+++ script/driver.rb
@@ -5,25 +5,23 @@ require_relative '../lib/csv_generator.rb'
 require 'csv'
 require 'pry'
 
-files = Dir['../data/acs/*.csv']
-total = Dir['../data/acs/*.csv'].length
-count = 0
-puts "#{total} Files Found"
+#to run - use $ bundle exec ruby driver.rb
 
-files.each do |file|
-  count += 1
-  csv_header_alteration(file)
-  p file
-  puts "File re-titled."
-  percentage = (count / total.to_f) * 100
-  puts "#{sprintf('%.2f', percentage)}% Complete"
-
+class Driver
+  def self.start
+    files = Dir['../data/acs/*.csv']
+    total = Dir['../data/acs/*.csv'].length
+    count = 0
+    puts "#{total} Files Found"
+    files.each do |file|
+      count += 1
+      csv_header_alteration(file)
+      p file
+      puts "File re-titled."
+      percentage = (count / total.to_f) * 100
+      puts "#{sprintf('%.2f', percentage)}% Complete"
+    end
+  end
 end
 
-
-
-
-
-
-
-
+Driver.start
